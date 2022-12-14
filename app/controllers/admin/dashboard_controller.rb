554cfcacfeaa2a16b523e679a4fae4181eeba003
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2022-12-14 21:21:38 +0000

    Display store stats on admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['HTTP_AUTH_USERNAME'], password: ENV['HTTP_AUTH_PASSWORD']
 
   def show
+    @product = Product.count
+    @category = Category.count
   end
 end
