29409dce856437f3647eeefa9c61ff91efd4ba57
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2022-12-24 03:54:02 +0000

    Define validation specs for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,16 @@
 class User < ApplicationRecord
 
   has_secure_password
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, 
+    presence: true,
+    uniqueness: { case_sensitive: false }
+  validates :password,
+    presence: true,
+    confirmation: true,
+    length: { minimum: 6 }
+  validates :password_confirmation, presence: true
   
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,73 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'Validations' do
+    before(:each) do
+      @user = User.create do |u|
+        u.first_name = 'First'
+        u.last_name = 'Last'
+        u.email = 'user@gmail.com'
+        u.password = 'Password'
+        u.password_confirmation = 'Password'
+      end
+    end
+
+    it "is avalid with all five required fields" do
+      expect(@user).to be_valid
+    end
+
+    it 'is not valid without a first name' do
+      @user.first_name = nil
+      expect(@user).to_not be_valid
+    end
+
+    it 'is not valid without a last name' do
+      @user.last_name = nil
+      expect(@user).to_not be_valid
+    end
+
+    it 'is not valid without an email' do
+      @user.email = nil
+      expect(@user).to_not be_valid
+    end
+
+    it 'is not valid without a password' do
+      @user.password = nil
+      expect(@user).to_not be_valid
+    end
+
+    it 'is not valid without a password_confirmation' do
+      @user.password_confirmation = nil
+      expect(@user).to_not be_valid
+    end
+
+    it 'is not valid when password and password_confirmation do not match' do
+      @user.password = 'Hello'
+      @user.password_confirmation = 'Password'
+      expect(@user).to_not be_valid
+    end
+
+    it 'is not valid when password is less than 6 characters' do
+      @user.password = 'Hello'
+      @user.password_confirmation = 'Hello'
+      expect(@user).to_not be_valid
+    end
+    
+    it 'is not valid when email is not unique' do
+      @user.save
+      
+      newUser = User.create do |u|
+        u.first_name = 'First'
+        u.last_name = 'Last'
+        u.email = 'USER@gmail.com'
+        u.password = 'Password'
+        u.password_confirmation = 'Password'
+      end
+
+      expect(newUser).to_not be_valid
+    end
+
+    
+
+  end
+end
